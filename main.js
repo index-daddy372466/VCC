c2ade5ca45152176058d99d35222cd8be2d04c2d
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2025-11-09 16:10:51 -0500

    feat: scroll feature after 100px from scrollTop

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const mobile_limit = 1010;
 // hamburger menu
 let hamburger = document.getElementById('hamburger-container')
 let hamburger_options = ['rotate','disappear']
-
-
+let [nav,mobile_nav] = [document.getElementById('nav'),document.getElementById('nav-mobile')]
+let header = document.querySelector('header')
 // click hamburger menu
 hamburger.addEventListener('click',handleHamburgerMenu) 
 
@@ -39,13 +39,25 @@ function handleHamburgerMenu(e) {
 
             console.log(get_s_class);
             children[i].classList.toggle(get_s_class)
-
+            
         }
+        if(children[1].classList.contains('s2-disappear')){
+            showMobileNav(mobile_nav)
 
+        } else {
+            hideMobileNav(mobile_nav)
+        }
     }
 }
 
-
+function hideMobileNav(nav,bool){
+    if(bool===false) return
+    nav.classList.add('hide-element')
+}
+function showMobileNav(nav,bool){
+    if(bool===false) return
+    nav.classList.remove('hide-element')
+}
 
 // setTimeout(()=>{
 //     document.getElementById('problem').classList.add('slide-section')
@@ -55,20 +67,33 @@ function handleHamburgerMenu(e) {
 
 
 window.onscroll = handleScroll
-
+let hr = document.getElementById('hr-primary')
 let sections =  [...document.querySelectorAll('.section-gen')]
-function handleScroll(e) {
-    let X = window.scrollX, Y = window.scrollY;
+function handleScroll() {
+    let X = window.scrollX, Y = window.scrollY
     
-
     // map sections
     sections.filter((_,i)=>i>0).map(section => {
         let sectionTop = section.getBoundingClientRect().y, sectionBottom = sectionTop + section.clientHeight;
-
-        if(Y < sectionTop) {
+        if(hr.getBoundingClientRect().y < sectionTop + 100) {
             section.classList.remove('appear-section')
         } else {
             section.classList.add('appear-section')
         }
     })
+
+    
+}
+
+
+function handleMobileNav(Y,header,nav){
+    // if scroll Y is greater than scrollTop + 100
+    if(Y > 100){
+        header.classList.add('fixed-position')
+        nav.classList.add('fixed-position')
+
+    } else {
+        header.classList.remove('fixed-position')
+        nav.classList.remove('fixed-position')
+    }
 }
