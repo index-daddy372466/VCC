8a5d2abaef181ceebaff98045cf25d03d7c9b25d
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2025-11-13 16:16:25 -0500

    feat: problem - data entered / review

--- main.js
+++ main.js
@@ -6,55 +6,24 @@ let hamburger_options = ['rotate','disappear']
 let [nav,mobile_nav] = [document.getElementById('nav'),document.getElementById('nav-mobile')]
 let nav_children = [...nav.children[0].children].map(x=>x.children[0]) // article elements
 let mobile_nav_children = [...mobile_nav.children[0].children].map(x=>x) // article elements
-console.log(mobile_nav_children)
 let header = document.querySelector('header')
+let problemcontainer = document.getElementById('problem')
+
+/* -------------------------- special paragraphs and phrases -------------------------- */
+let rotatepara = document.querySelector(".rotate-para");
+
+
+
+/* -------------------------- special paragraphs and phrases -------------------------- */
+
+// window event - scroll
+window.onscroll = handleScroll
+
+// appear section on load
+document.getElementById('problem').classList.remove('appear-section')
+
 // click hamburger menu
 hamburger.addEventListener('click',handleHamburgerMenu) 
-
-let problemcontainer = document.getElementById('problem')
-// handle hamburger menu
-function handleHamburgerMenu(e) { 
-    let hamburger_children = [...hamburger.children] || [...e.currentTarget.children]
-    if(document.body.clientWidth < mobile_limit) {
-        let children = [...hamburger_children]
-        console.log(children)
-        for(let i = 0; i < children.length; i++) {
-
-            let get_s_class // instantiate variable for s class
-            switch(true){
-                case i%2 == 0:
-                get_s_class = [...children[i].classList].find(x=>/^s\d{1}$/gi.test(x))
-                console.log(get_s_class)
-                get_s_class += `-${hamburger_options[i%2]}`
-                break;
-                case i%2 == 1:
-                get_s_class = [...children[i].classList].find(x=>/^s\d{1}$/gi.test(x))
-                console.log(get_s_class)
-                get_s_class += `-${hamburger_options[i%2]}`
-                break;
-
-                default:console.log(undefined)
-                break;
-            }
-
-            console.log(get_s_class);
-            children[i].classList.toggle(get_s_class)
-            
-        }
-        if(children[1].classList.contains('s2-disappear')){
-            showMobileNav(mobile_nav)
-            hamburger_children.map(child => child.classList.remove('disabled'))
-            hamburger_children.map(child => child.classList.add('enabled'))
-
-        } else {
-            hideMobileNav(mobile_nav)
-            hamburger_children.map(child => child.classList.remove('enabled'))
-            hamburger_children.map(child => child.classList.add('disabled'))
-        }
-    }
-}
-
-// click on a navitem in hamburger menu
 if(document.body.clientWidth < 1010){
     let nav_children = [...mobile_nav.children]
     nav_children.map(child => {
@@ -62,87 +31,131 @@ if(document.body.clientWidth < 1010){
     })
 }
 
-function hideMobileNav(nav,bool){
-    if(bool===false) return
-    nav.classList.add('hide-element')
-}
-function showMobileNav(nav,bool){
-    if(bool===false) return
-    nav.classList.remove('hide-element')
-}
-
-document.getElementById('problem').classList.remove('appear-section')
-
-window.onscroll = handleScroll
-let hr = document.getElementById('hr-primary')
-let sections =  [...document.querySelectorAll('.section-gen')]
-let sections_gen = [...document.querySelectorAll('.section-gen-link')]
-let target_section
-let background_pos = {x:undefined,y:undefined}
-let backgroundcounter = 0;
-
-let percentage = .57;
-let starting = 200;
-let about_bg = 'center end'
-// handle scroll
-function handleScroll() {
-    let Y = window.scrollY
-    
-    // map sections
-    sections.filter((_,p) => p > 0).map(section => {
-        let sectionTop = section.getBoundingClientRect().y, sectionBottom = sectionTop + section.clientHeight;
-        if(hr.getBoundingClientRect().y < sectionTop + 100) {
-            section.classList.remove('appear-section');
-        } else {
-            section.classList.add('appear-section')
-        }
-    })
-
-     for(let i = 0; i < sections_gen.length; i++){
-        nav_children[i].classList.remove('target-link') // remove target link from nav list items 
-        if(mobile_nav_children[i])mobile_nav_children[i].classList.remove('target-link')
 
 
-        let sectionTop = sections_gen[i].getBoundingClientRect().y, sectionBottom = sectionTop + sections_gen[i].clientHeight;
-        if((hr.getBoundingClientRect().y > sectionTop) && hr.getBoundingClientRect().y < sectionBottom ||
-            (i==sections_gen.length - 1 && hr.getBoundingClientRect().y > sectionBottom)) {
-            target_section = sections_gen[i]
-            let nav_element = nav_children[i]
-            let mobile_nav_element = mobile_nav_children[i]
-            nav_element.classList.add('target-link')
-            mobile_nav_element.classList.add('target-link');
-            
-            backgroundcounter = Y
-            // problem container - manipulate background image 
-            if(nav_element.getAttribute('href')==='#problem'){ 
-                    background_pos.x = +(backgroundcounter*(percentage))*-1
-                    // console.log(background_pos)
-                    sections_gen[i].style.backgroundPosition = background_pos.x + "px";
-            }
-            // else if(/^#(faq|services)$/.test(nav_element.getAttribute('href'))){ 
-            //     starting--
-            //     sections_gen[i].style.backgroundPositionY = (starting*.49) + "px";
-            // }
-            else {
-                if(backgroundcounter >= 0) {
-                    backgroundcounter--
+
+
+//|   handle scroll variables
+/*|*/   let hr = document.getElementById('hr-primary')
+/*|*/   let sections =  [...document.querySelectorAll('.section-gen')]
+/*|*/   let sections_gen = [...document.querySelectorAll('.section-gen-link')]
+/*|*/   let target_section
+/*|*/   let background_pos = {x:undefined,y:undefined}
+/*|*/   let backgroundcounter = 0;
+/*|*/  
+/*|*/   let percentage = .57;
+/*|*/   let starting = 200;
+/*|*/   let about_bg = 'center end'
+
+/*|*/// functions 
+/*|*/     function handleHamburgerMenu(e) { 
+        let hamburger_children = [...hamburger.children] || [...e.currentTarget.children]
+        if(document.body.clientWidth < mobile_limit) {
+            let children = [...hamburger_children]
+            for(let i = 0; i < children.length; i++) {
+
+                let get_s_class // instantiate variable for s class
+                switch(true){
+                    case i%2 == 0:
+                    get_s_class = [...children[i].classList].find(x=>/^s\d{1}$/gi.test(x))
+                    console.log(get_s_class)
+                    get_s_class += `-${hamburger_options[i%2]}`
+                    break;
+                    case i%2 == 1:
+                    get_s_class = [...children[i].classList].find(x=>/^s\d{1}$/gi.test(x))
+                    console.log(get_s_class)
+                    get_s_class += `-${hamburger_options[i%2]}`
+                    break;
+
+                    default:console.log(undefined)
+                    break;
                 }
-               
-                starting = 200
+
+                console.log(get_s_class);
+                children[i].classList.toggle(get_s_class)
+                
+            }
+            if(children[1].classList.contains('s2-disappear')){
+                showMobileNav(mobile_nav)
+                hamburger_children.map(child => child.classList.remove('disabled'))
+                hamburger_children.map(child => child.classList.add('enabled'))
+
+            } else {
+                hideMobileNav(mobile_nav)
+                hamburger_children.map(child => child.classList.remove('enabled'))
+                hamburger_children.map(child => child.classList.add('disabled'))
             }
         }
-     }
+        }
+/*|*/     function hideMobileNav(nav,bool){
+        if(bool===false) return
+        nav.classList.add('hide-element')
+        }
+/*|*/     function showMobileNav(nav,bool){
+        if(bool===false) return
+        nav.classList.remove('hide-element')
+        }
+/*===> */ function handleScroll() {
+        let Y = window.scrollY
+        
+        // map sections
+        sections.filter((_,p) => p > 0).map(section => {
+            let sectionTop = section.getBoundingClientRect().y, sectionBottom = sectionTop + section.clientHeight;
+            if(hr.getBoundingClientRect().y < sectionTop + 100) {
+                section.classList.remove('appear-section');
+            } else {
+                section.classList.add('appear-section')
+            }
+        })
 
-    // if y > top
-    if(Y > document.body.scrollTop){
-        mobile_nav.classList.remove('relative-nav')
-        mobile_nav.classList.add('fixed-nav')
-    } else {
-        mobile_nav.classList.remove('fixed-nav')
-        mobile_nav.classList.add('relative-nav')
+        for(let i = 0; i < sections_gen.length; i++){
+            nav_children[i].classList.remove('target-link') // remove target link from nav list items 
+            if(mobile_nav_children[i])mobile_nav_children[i].classList.remove('target-link')
+
+
+            let sectionTop = sections_gen[i].getBoundingClientRect().y, sectionBottom = sectionTop + sections_gen[i].clientHeight;
+            if((hr.getBoundingClientRect().y > sectionTop) && hr.getBoundingClientRect().y < sectionBottom ||
+                (i==sections_gen.length - 1 && hr.getBoundingClientRect().y > sectionBottom)) {
+                target_section = sections_gen[i]
+                let nav_element = nav_children[i]
+                let mobile_nav_element = mobile_nav_children[i]
+                nav_element.classList.add('target-link')
+                mobile_nav_element.classList.add('target-link');
+                
+                backgroundcounter = Y
+                // problem container - manipulate background image 
+                if(nav_element.getAttribute('href')==='#problem'){ 
+                        background_pos.x = +(backgroundcounter*(percentage))*-1
+                        // console.log(background_pos)
+                        sections_gen[i].style.backgroundPosition = background_pos.x + "px";
+                        sections_gen[i].style.backgroundColor = '#464f58ba';
+                }
+           
+                else {
+                    if(backgroundcounter >= 0) {
+                        backgroundcounter--
+                    }
+                    // if(starting_rotation < 20) {
+                    //     starting_rotation++
+                    //     rotatepara.style = `transform:rotate(${starting_rotation}deg)`
+                    // }
+                
+                    starting = 200
+                }
+            }
+        }
+
+        // if y > top
+        if(Y > document.body.scrollTop){
+            mobile_nav.classList.remove('relative-nav')
+            mobile_nav.classList.add('fixed-nav')
+        } else {
+            mobile_nav.classList.remove('fixed-nav')
+            mobile_nav.classList.add('relative-nav')
+        }
+
+
+        
+        
     }
 
-
-    
-    
-}
